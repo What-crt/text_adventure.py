17f6f69740f44cbcb28b4af984930755aa742e7c
What-crt <mykel.allan@ucas-edu.net>
2025-09-25 19:43:44 +0000

    work on text adventure story Co-authored-by: Merp <eliasperkins-jpg@users.noreply.github.com> Co-authored-by: luvspink22 <luvspink22@users.noreply.github.com> Co-authored-by: ruberducky767 <ruberducky767@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+# MA AH EP DG 6th Final-text adventure
 
+#Mykel's part
+name = input("What is your name?")
+print(f"Hello {name}! welcome to 'Text Adventure'. ")
+
+
+print("(game start)")
+#Ambar's part
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Elias' part
+hp = 3
+
+if hp <= 0:
+    print("You Lose... Wanna play again? Press one to retry")
+    if input == 1:
+        function return #restart code
+
+#Domininiks part
