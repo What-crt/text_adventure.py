2b3484c3533b5973439716d239a4cab7759504ed
What-crt <mykel.allan@ucas-edu.net>
2025-09-26 19:41:42 +0000

    work on text adventure game.
    Co-authored-by: luvspink22 <luvspink22@users.noreply.github.com>
    Co-authored-by: ruberducky767 <ruberducky767@users.noreply.github.com>
    Co-authored-by: Merp <eliasperkins-jpg@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,19 +2,38 @@
 
 #Mykel's part
 name = input("What is your name?")
-print(f"Hello {name}! welcome to 'Text Adventure'. ")
-
-
-print("(game start)")
+print(f"Hello {name}! welcome to the tutorial for 'Text Adventure'! ")
+print("When asked a question, you will awnser with your perferded option. These choices will influence your adventure.")
+print("For Exaple:")
+answer= int(input("You see two shops. A weapon shop and food market. You only have enough money to buy one thing. Do you want to buy a weapon or food?"))
+if answer == "weapon":
+    print("Nice! you bought yourself a new sword!")
+else:
+    if answer == "food":
+        print("yum! You bought a sack of apples!")
+print(f"Nice choice {name}!")
+print("Now that know how the your adventure will work you are ready!")
+print("When you were young, you heard myths of an ancient golden egg that provides vast healing capabilities. Now that you're finally old enough, you will begin the search for it, whether it's myth or not.")
+print("Warning: You have been given three lives. If you lose all three you will be forced to restart.")
+print("Good luck brave adventurer!")
 #Ambar's part
 
+answer = int(input("You stumble across two paths, one is a bright path and the other a dark path. What path will you choose? "))
+if answer == "Bright path":
+    print("YouDarka backery")
+else:
+!    i
+    if answer == ""f answer == "Dark Path":
+    
+Bright path        print
 
-
-
-
-
-
-
+        inp
+ut("Fight or Run, Press 1 to fight, 2 to run")
+ {}m)
+    input = 1
+        if input = 1
+        elif input = 2
+            if         
 
 
 
@@ -27,11 +46,50 @@ print("(game start)")
 
 
 #Elias' part
-hp = 3
+base_buff = 0
+use_super_buff = 2
+buff = 1
+current_buff = 0
 
-if hp <= 0:
-    print("You Lose... Wanna play again? Press one to retry")
-    if input == 1:
-        function return #restart code
+
+
+#current_buff += buff
+
+#current_buff += use_super_buff
+
+
+
+base_hp = 3
+hp = base_hp + current_buff
+if current_buff == 0:
+    print("You have run out of buff")
+elif hp < 0:
+    if hp <= 3:
+        base_hp - 3
+    elif hp == -2:
+        base_hp - 2
+    else:
+        base_hp - 1
+
+if base_hp <= 0 :
+    print("You Lose... Wanna play again?")
+elif base_hp > 5:
+    print ("You win!!!")
+#atk = 1
+#buff = 1
+#charge = 2
+#super_buff = 2
+
+#player_atk = 1
+#player_charge = 3
 
 #Domininiks part
+print("you find a jungle temple do you want to go in or go around")
+yesno= input("yes or no")
+             
+if yesno == "yes": 
+    print("you find a giant spider")
+
+else:
+    print("yoy fell")
+
